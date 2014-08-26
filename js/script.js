1e0279f91b420cdc368881935cbd1a14077304f9
Eunice Kokor <eunicekokor@gmail.com>
2014-08-26 15:31:31 -0700

    updated descriptions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+var adjectives={
+  1: "CS Student at Columbia Engineering",
+  2: "kid at heart",
+  3: "innovator in spirit",
+  4: "lover of all things tech & STEM"
+};
+
+var newsent;
+var ready = true;
+
+$(document).ready(function (){
+
+$('#data1').fadeIn('fast').delay(1000).fadeOut('fast').html(adjectives[1]);
+
+$('#data2').delay(1500).fadeIn('fast').delay(1500).fadeOut('fast').html(adjectives[2]);
+
+$('#data3').delay(3500).fadeIn('fast').delay(1500).fadeOut('fast').html(adjectives[3]);
+
+$('#data4').delay(5500).fadeIn('fast').html(adjectives[4]);
+
 });
